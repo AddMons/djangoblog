a0f0987712731e3a2ea8f94c9b93183870763bfe
AddMons <50752213+AddMons@users.noreply.github.com>
2020-11-26 23:24:07 +0200

    set4

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(get_wsgi_application())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
